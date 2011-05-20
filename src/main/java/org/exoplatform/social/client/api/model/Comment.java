d4f9417e690c11391e415cc7c61d04d40ee6cfde
Hoat Le Van <hoatlevan@gmail.com>
2011-05-20 18:25:44 +0700

    Comment APIs: Completed

--- src/main/java/org/exoplatform/social/client/api/model/Comment.java
+++ src/main/java/org/exoplatform/social/client/api/model/Comment.java
@@ -23,8 +23,125 @@ package org.exoplatform.social.client.api.model;
  * @since May 19, 2011
  */
 public interface Comment extends Model {
+
+  /**
+   * The fields that represent the Comment object in json form.
+   *
+   * <p>
+   * All of the fields that comments can have.
+   * </p>
+   *
+   */
+  public static enum Field {
+    ID("id"),
+    /** the json field for userId. */
+    USER_ID("userId"),
+    /** the json field for activityId. */
+    ACTIVITY_ID("activityId"),
+     /** the json field for streamTitle. */
+    CONTENT("content"),
+    /** the json field for postedTime. */
+    POSTED_TIME("postedTime");
+
+    /**
+     * The json field that the instance represents.
+     */
+    private final String jsonString;
+
+    /**
+     * create a field base on the a json element.
+     *
+     * @param jsonString the name of the element
+     */
+    private Field(String jsonString) {
+      this.jsonString = jsonString;
+    }
+
+    /**
+     * emit the field as a json element.
+     *
+     * @return the field name
+     */
+    @Override
+    public String toString() {
+      return jsonString;
+    }
+  }
+
+  /**
+   * Gets the comment id.
+   *
+   * @return the comment id
+   */
+  String getId();
+
+  /**
+   * Sets the comment id.
+   *
+   * @param id the comment id
+   */
+  void setId(String id);
+
+  /**
+   * Gets user id who posted this comment.
+   *
+   * @return the user id
+   */
+  String getUserId();
+
+  /**
+   * Sets user id who posted this comment.
+   *
+   * @param userId the user id
+   */
+  void setUserId(String userId);
+
+  /**
+   * Gets the activity id which is associated with this comment.
+   *
+   * @return the activity id.
+   */
+  String getActivityId();
+
+  /**
+   * Sets the activity which is associated with this comment.
+   *
+   * @param activityId the activity id
+   */
+  void setActivityId(String activityId);
+
+  /**
+   * Gets the comment content.
+   *
+   * @return the comment content.
+   */
+  String getContent();
+
+  /**
+   * Sets the comment content.
+   *
+   * @param content the comment content
+   */
+  void setContent(String content);
+
+  /**
+   * Gets the posted time of this comment as timestamp value.
+   *
+   * @return the posted time of this comment
+   */
+  Long getPostedTime();
+
+  /**
+   * Sets the posted time of this comment as timestamp value.
+   *
+   * @param postedTime the posted time of this comment.
+   */
+  void setPostedTime(Long postedTime);
+
   /**
    * Gets the activity is associated with this comment.
+   *
+   * This must be lazy loading for better performance.
    *
    * @return the activity
    */
@@ -32,6 +149,8 @@ public interface Comment extends Model {
 
   /**
    * Gets the identity who commented.
+   *
+   * This must be lazy loading for better performance.
    *
    * @return the identity
    */
