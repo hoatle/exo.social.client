c101cbd9047e9e9fe678449ed67d0661c3ed3d1a
Hoat Le <hoatlevan@gmail.com>
2011-06-07 01:16:02 +0700

    Correct copyright header file

--- src/main/java/org/exoplatform/social/client/api/Client.java
+++ src/main/java/org/exoplatform/social/client/api/Client.java
@@ -1,26 +1,25 @@
 /*
  * Copyright (C) 2003-2011 eXo Platform SAS.
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Affero General Public License
- * as published by the Free Software Foundation; either version 3
- * of the License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.exoplatform.social.client.api;
 
 import org.exoplatform.social.client.api.auth.AccessDeniedException;
-import org.exoplatform.social.client.api.auth.AuthSchema;
+import org.exoplatform.social.client.api.service.ActivityService;
 import org.exoplatform.social.client.api.service.ConnectionService;
 import org.exoplatform.social.client.api.service.IdentityService;
-import org.exoplatform.social.client.api.service.ActivityService;
 
 /**
  * The main entry point to get eXo Social Services.
